51a89e4bf3debc05439318f9acf79ef3d5181c43
Stephanie Vaca <80546663+stephanievaca@users.noreply.github.com>
2022-12-14 14:19:29 -0800

    Update AircraftCarrier.h

--- AircraftCarrier.h
+++ AircraftCarrier.h
@@ -1,6 +1,6 @@
 /**
  * @file Square.h
- * @author Michelle Tran, Stephanic Vaca, Kyle Kiyuna
+ * @author Michelle Tran, Stephanie Vaca, Kyle Kiyuna
  * @brief user input class implementation file
  * @version 0.1
  * @date 2022-11-21
