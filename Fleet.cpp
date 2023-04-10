da3b35724d64c2f9fdcb2a851a4c7fa67025ee44
mushie4282 <tranmichelle0826@yahoo.com>
2023-04-10 00:24:02 -0700

    Organized Files

--- Fleet.cpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file Fleet.cpp
- * @author Michelle Tran, Stephanic Vaca, Kyle Kiyuna
- * @brief user input class implementation file
- * @version 0.1
- * @date 2022-11-21
- * 
- */
-
--- Submarine.h
+++
@@ -1,8 +0,0 @@
-/**
- * @file Square.h
- * @author Michelle Tran, Stephanic Vaca, Kyle Kiyuna
- * @brief user input class implementation file
- * @version 0.1
- * @date 2022-11-21
- * 
- */
